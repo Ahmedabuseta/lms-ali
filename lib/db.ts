feef7e8f5b91380b82063b39c3ac1c76c86635c0
Ahmed6 <133219271+Ahmedabuseta@users.noreply.github.com>
2025-06-22 18:39:51 -0500

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,32 +1,32 @@
-// // import { PrismaClient } from '@prisma/client/edge';
-// import { withAccelerate } from '@prisma/extension-accelerate';
-// import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaClient } from '@prisma/client'
 
-// // Create and extend the client first
-// const client = new PrismaClient().$extends(withAccelerate());
+// Create and extend the client first
+const client = new PrismaClient().$extends(withAccelerate());
 
-// // Use `typeof client` to infer the correct extended type
-// type AcceleratedPrismaClient = typeof client;
+// Use `typeof client` to infer the correct extended type
+type AcceleratedPrismaClient = typeof client;
 
-// declare global { // eslint-disable-next-line no-var
-//   var prisma: AcceleratedPrismaClient | undefined; }
+declare global { // eslint-disable-next-line no-var
+  var prisma: AcceleratedPrismaClient | undefined; }
 
-// export const db: AcceleratedPrismaClient = globalThis.prisma ?? client;
-
-// if (process.env.NODE_ENV !== 'production') {
-//   globalThis.prisma = db;
-// }
-import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error', 'warn'],
-  });
+export const db: AcceleratedPrismaClient = globalThis.prisma ?? client;
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = db;
-}
+  globalThis.prisma = db;
+// }
+// import { PrismaClient } from '@prisma/client';
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// export const db =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ['error', 'warn'],
+//   });
+
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = db;
+// }
 
